42552bc31b2e63a5d6a4c78b5b43583a5e9d07a3
dlowicki <44500521+dlowicki@users.noreply.github.com>
2019-05-17 14:37:44 +0200

    Update 3 17.05.2019

--- src/de/lowicki/versionen/manager/Load.java
+++ src/de/lowicki/versionen/manager/Load.java
@@ -12,7 +12,6 @@ public class Load
   
   public Load() {
 	  loadConfig();
-	  loadVersionen();
   }
   
   private void loadConfig() {
@@ -20,19 +19,8 @@ public class Load
 	    	new existsFile(path);
 	    	Wini ini = new Wini(new File(path));
 	    	Main.connectionURL = ini.get("Connection", "connection");
-	    	
-	    	System.out.println("[Load] Die ConnectionURL wurde geladen");
-	    } catch (Exception e) {
-	      e.printStackTrace();
-	    }
-  }
-  
-  private void loadVersionen() {
-	    try { 
-	    	new existsFile(path);
-	    	Wini ini = new Wini(new File(path));
 	    	Main.urls.putAll((ini.get("url")));
-	    	System.out.println("[Load] Die URLS wurden geladen");
+	    	System.out.println("[Load] Die ConnectionURL und die URLS wurden geladen");
 	    } catch (Exception e) {
 	      e.printStackTrace();
 	    }
