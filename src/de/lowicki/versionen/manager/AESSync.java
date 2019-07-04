c19becc319e66d3dcad3f1f182fc6eba9bf5ec21
dlowicki <44500521+dlowicki@users.noreply.github.com>
2019-07-04 09:28:29 +0200

    Version 6 04.07.2019

---
+++ src/de/lowicki/versionen/manager/AESSync.java
@@ -0,0 +1,37 @@
+package de.lowicki.versionen.manager;
+
+import java.security.Key;
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+
+public class AESSync {
+	
+    private static final String ALGO = "AES";
+    private static final byte[] keyValue = new byte[]{'T', 'h', 'e', 'B', 'e', 's', 't', 'S', 'e', 'c', 'r', 'e', 't', 'K', 'e', 'y'};
+
+    public String encrypt(String data) throws Exception {
+        Key key = generateKey();
+        Cipher c = Cipher.getInstance(ALGO);
+        c.init(Cipher.ENCRYPT_MODE, key);
+        byte[] encVal = c.doFinal(data.getBytes());
+        return Base64.getEncoder().encodeToString((encVal));
+    }
+
+    public String decrypt(String encryptedData) throws Exception {
+        Key key = generateKey();
+        Cipher c = Cipher.getInstance(ALGO);
+        c.init(Cipher.DECRYPT_MODE, key);
+        byte[] decordedValue = Base64.getDecoder().decode(encryptedData);
+        byte[] decValue = c.doFinal(decordedValue);
+        System.out.println("[AESSync] Verschlüsselung aktiviert");
+        return new String(decValue);
+    }
+
+    private static Key generateKey() throws Exception {
+        return new SecretKeySpec(keyValue, ALGO);
+    }
+    
+
+}
